78201606eae2cbe3a832d00beb6eb6c30b232763
KS <kyusang.dev@gmail.com>
2025-02-10 15:35:42 +0900

    named 파라미터 함수 호출: 이름이 지정된 인사를 출력하는 함수 추가 및 기본값 설정

--- 05-Dart and Flutter/03-function.dart
+++ 05-Dart and Flutter/03-function.dart
@@ -5,6 +5,18 @@ void main() {
   getCurrentDateTime();
   var hourDifference = -7;
   getCurrentDateTimeWithDifference(hourDifference);
+
+  printGreetingNamed();
+  printGreetingNamed(personName: "Rich");
+  printGreetingNamed(personName: "Mary", clientId: 001);
+}
+
+void printGreetingNamed({String personName = 'Stranger', int clientId = 999}) {
+  if (personName.contains('Stranger')) {
+    print('Employee: $clientId, Stranger danger!');
+  } else {
+    print('Employee: $clientId, Hello $personName');
+  }
 }
 
 // 함수 선언
