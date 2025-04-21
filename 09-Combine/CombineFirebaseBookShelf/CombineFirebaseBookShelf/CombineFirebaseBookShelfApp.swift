192d22fadf239805052ed71a669b0783cadbb6bd
KS <kyusang.dev@gmail.com>
2025-04-21 09:44:38 +0900

    Sync

---
+++ 09-Combine/CombineFirebaseBookShelf/CombineFirebaseBookShelf/CombineFirebaseBookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFirebaseBookShelfApp.swift
+//  CombineFirebaseBookShelf
+//
+//  Created by 황규상 on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineFirebaseBookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
